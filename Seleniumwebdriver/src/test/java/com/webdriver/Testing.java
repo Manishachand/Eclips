72ad8d33dbc567bc33b30e1fc25c826aa3717706
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-43RRX52.dir.svc.accenture.com>
2018-07-18 11:41:14 +0530

    Test commit

--- Seleniumwebdriver/src/test/java/com/webdriver/Testing.java
+++ Seleniumwebdriver/src/test/java/com/webdriver/Testing.java
@@ -4,15 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Testing {
+	@Parameters("browser")
 @Test
-	public void testlogin()
+	public void testlogin(String value)
 	{
+		WebDriver driver=null;
+		if(value.equals("chrome"))
+		{
 	System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver.exe");
-	WebDriver driver=new ChromeDriver();
+	driver=new ChromeDriver();
+		}
+		else if(value.equals("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver","C:\\Program Files\\geckodriver.exe");
+			driver=new FirefoxDriver();
+		}
+		else
+			System.out.println("Not browser found");
 	driver.get("https://github.com/login");
 	driver.findElement(By.id("login_field")).sendKeys("Manishachand");
 	driver.findElement(By.id("password")).sendKeys("Shiya@888");
@@ -21,7 +35,7 @@ public class Testing {
 	System.out.println(title);
 
 
-Assert.assertEquals(title, "Sign in to GitHub · GitHub");
+Assert.assertEquals(title, "GitHub");
 
 System.out.println("in test modify");
 	
